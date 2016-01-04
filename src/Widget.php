6c7ddc09a9b8a4a48ad98e28803bff2ef146af4d
nikel303 <nikel303@ya.ru>
2016-01-04 17:58:12 +0300

    refactor widget

--- src/Widget.php
+++ src/Widget.php
@@ -10,14 +10,33 @@ class Widget extends \CInputWidget
 	public $lang = 'ru';
 	public $size = 'normal';
 	public $theme = 'light';
+
 	public $jsCallback;
 
 	public function run()
 	{
 
-		\Yii::app()->clientScript->registerScriptFile('https://www.google.com/recaptcha/api.js?hl=' . $this->lang, \CClientScript::POS_END);
+		if (empty($this->publicKey))
+			throw new CException(Yii::t('yii', 'Property YiiRecaptcha2\Widget.publicKey cannot be empty.'));
 
-		$this->customFieldPrepare();
+		list($name, $id) = $this->resolveNameID();
+
+		if (isset($this->htmlOptions['id']))
+			$id = $this->htmlOptions['id'];
+		else
+			$this->htmlOptions['id'] = $id;
+
+		if (isset($this->htmlOptions['name']))
+			$name = $this->htmlOptions['name'];
+
+		$this->registerClientScript();
+
+		$this->htmlOptions['value'] = null;
+
+		if ($this->hasModel())
+			echo CHtml::activeHiddenField($this->model, $this->attribute, $this->htmlOptions);
+		else
+			echo CHtml::hiddenField($name, $this->value, $this->htmlOptions);
 
 		echo \CHtml::tag('div', [
 			'class' => 'g-recaptcha',
@@ -28,23 +47,23 @@ class Widget extends \CInputWidget
 		], '', true);
 	}
 
-	protected function customFieldPrepare()
+	protected function registerClientScript()
 	{
 
-		echo \CHtml::activeHiddenField($this->model, $this->attribute, ['value' => '']);
+		$cs = \Yii::app()->clientScript;
 
-		$inputId = \CHtml::activeId($this->model, $this->attribute);
+		$cs->registerScriptFile('https://www.google.com/recaptcha/api.js?hl=' . $this->lang, \CClientScript::POS_END);
+
+		$id = $this->htmlOptions['id'];
 
 		if (empty($this->jsCallback)) {
-			$jsCode = "var recaptchaCallback_{$inputId} = function(response){jQuery('#{$inputId}').val(response);};";
+			$jsCode = "var recaptchaCallback_{$id} = function(response){jQuery('#{$id}').val(response);};";
 		} else {
-			$jsCode = "var recaptchaCallback_{$inputId} = function(response){jQuery('#{$inputId}').val(response); {$this->jsCallback}(response);};";
+			$jsCode = "var recaptchaCallback_{$id} = function(response){jQuery('#{$id}').val(response); {$this->jsCallback}(response);};";
 		}
 
-		$this->jsCallback = "recaptchaCallback_{$inputId}";
-
-		\Yii::app()->clientScript->registerScript("recaptchaCallback_{$inputId}", $jsCode, \CClientScript::POS_END);
-
+		$this->jsCallback = "recaptchaCallback_{$id}";
+		$cs->registerScript("recaptchaCallback_{$id}", $jsCode, \CClientScript::POS_END);
 	}
 
 }
